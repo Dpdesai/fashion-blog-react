89e8c9818795c3758318b1f3d0e5efbcde0c5064
Divya Desai <d1479desai@gmail.com>
2023-01-24 21:40:13 -0500

    fashion blog intial commit

---
+++ src/styles.js
@@ -0,0 +1,42 @@
+// import styled from "styled-components";
+
+// export const Nav = styled.nav`
+//   ul {
+//     list-style-type: none;
+//     display: flex;
+//     justify-content: space-between;
+//     padding: 0;
+//     a {
+//       color: lightgray;
+//     }
+//   }
+// `;
+
+// export const Article = styled.article`
+//   padding-bottom: 1rem;
+//   border-bottom: 1px solid lightgray;
+//   margin-bottom: 4rem;
+//   time {
+//     color: lightgray;
+//   }
+//   img {
+//     width: 100%;
+//   }
+//   h3 {
+//     font-weight: 400;
+//     margin-top: 5px;
+//     margin-bottom: 10px;
+//     font-size: 2em;
+//   }
+//   .content {
+//     color: darkgray;
+//     font-size: 16px;
+//     line-height: 24px;
+//     &::first-letter {
+//       font-size: 5em;
+//       padding: 20px 3px 16px;
+//       margin-right: 6px;
+//       float: left;
+//     }
+//   }
+// `;
